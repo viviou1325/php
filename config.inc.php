a690a27431ab3f6521538bdfa9a1e8d7336ccaa5
Unknown <garcia.xavier@live.fr>
2018-01-04 11:32:11 +0100

    config.inc index.php
    ajout de config.inc modification de index.php

---
+++ config.inc.php
@@ -0,0 +1,9 @@
+<?php
+	//$_PAGES=array("HOME_PAGE" => "./index.php");
+	//Go cours php chap5 pages 47~50
+$PAGES = array( 'accueil' => 'accueil', 'connexion'=> 'connexion', 'profil'=>'profil' , 'recette_detail'=>'recette_detail','recette_liste'=>'recette_liste','inscription');
+
+$HOME_PAGE = 'accueil';
+$ERROR_404 = 'accueil';
+?>
+	
--- index.php
+++ index.php
@@ -1,9 +1,40 @@
 <?php
-	require_once('./application/libraries/smarty-3.1.30/libs/Smarty.class.php');
+	require_once('application/libraries/smarty-3.1.30/libs/Smarty.class.php');
 
-	$smarty = new smarty;
+	///$smarty = new smarty;
 
 	//$smarty->display("./application/vue/layout.tpl");
-	$smarty->display("./application/vue/modules/accueil.tpl");
+	///$smarty->display("./application/vue/modules/accueil.tpl");
+	
+	require_once('config.inc.php');
+    //require_once('./application/librairies/smarty-3.1.30/libs/Smarty.class.php');
+
+    $data = array();
+
+    $current_page = $_GET['page'];
+
+    if (!in_array($current_page, $PAGES)) 
+	{
+       	if ($current_page=='') 
+	{
+           	 $current_page=$HOME_PAGE;
+        } 
+	else 
+	{
+            echo 'Erreur 404';
+            //$current_page=$ERROR_404;
+        }
+    }
+
+    require_once("application/modele/".$current_page.".inc.php");
+
+    $smarty = new Smarty();
+    $smarty->caching = 0;
+    $smarty->setTemplateDir('application/views/');
+    foreach ($data as $key => $value) {
+        $smarty->assign($key, $value);
+    }
+    $smarty->display("application/vue/".$current_page.".tpl");
+    $smarty->display("application/vue/modules".$current_page.".tpl");
 
 ?>
