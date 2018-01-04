a3ff9ec883cdfb1d837e7c26e798fc30d6f589df
PanneCake <alexandre@linux.home>
2018-01-04 14:42:45 +0100

    ajout configBDD.inc.php

---
+++ configBDD.inc.php
@@ -0,0 +1,25 @@
+<?php
+
+define('DB_HOST', 'mysql-flack.alwaysdata.net');
+define("DB_PORT", 3306);
+define('DB_DATABASE', 'flack_php');
+define('DB_USERNAME', 'flack');
+define('DB_PASSWORD', 'Cookies');
+
+//$PDO_BDD  new PDO('mysql:host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_DATABASE, DB_USERNAME, DB_PASSWORD);
+
+try
+{
+	$PDO_BDD = new PDO('mysql:host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_DATABASE, DB_USERNAME, DB_PASSWORD);
+	$PDO_BDD->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$PDO_BDD->exec("SET NAMES 'utf8'");
+}
+
+catch(Exception $e)
+{
+	echo 'Erreur : '.$e->getMessage().'<br/>';
+	echo 'No : '.$e->getCode();
+	exit();
+}
+
+?>
