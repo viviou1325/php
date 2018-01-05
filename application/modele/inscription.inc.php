1e2e6eecd7ef933b8279f421f9db2a842393c26b
PanneCake <rouxalexandre04@gmail.com>
2018-01-05 14:32:05 +0100

    modif inscription.inc.php

---
+++ application/modele/inscription.inc.php
@@ -0,0 +1,17 @@
+<?php
+
+	/*$plop = 0;
+			if
+			(
+				isset($_REQUEST['send']) && (!empty($_REQUEST['login'])) &&
+				(!empty($_REQUEST['last_name'])) &&
+				(!empty($_REQUEST['first_name'])) &&
+				(!empty($_REQUEST['mail']) && filter_var($_REQUEST['mail'], FILTER_VALIDATE_EMAIL) ) &&
+				(!empty($_REQUEST['pwd'])) &&
+				($_REQUEST['cfpwd'] == $_REQUEST['pwd'])
+			)
+			{*/
+				//$plop=1;
+			//}
+
+?>
