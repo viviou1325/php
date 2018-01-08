7095501a24e2baf3ffc60af432682335772422c2
Unknown <garcia.xavier@live.fr>
2018-01-08 01:38:14 +0100

    vbbffgfggfbfffg
    gfgf

--- application/modele/accueil.inc.php
+++ application/modele/accueil.inc.php
@@ -1,12 +1,11 @@
 <?php  
 
-$req_accueil = $PDO_BDD->query('SELECT * FROM T_CATEGORIE_CAT')
+$req_accueil = $PDO_BDD->query('SELECT * FROM T_CATEGORIE_CAT');
 
 $aff_req = $req_accueil->fetchAll(PDO::FETCH_ASSOC);
 
 foreach($aff_req as $line)
 {
-  echo '<h2>'.$line['CAT_LABEL'].'</h2> <br/> <img src="application/media/categorie/'.$line['CAT_ILLUSTRATION'].'"'>;
+  echo '<h2>'.$line['CAT_LABEL'].'</h2> <br/> <img src="media/categorie/'.$line['CAT_ILLUSTRATION'].'">';
 }
-
 ?>
